41807be28e927087f7bd022046c1bac5eddbbfd6
张曦 <972243884@qq.com>
2019-10-19 10:00:50 +0800

    1

---
+++ README.md
@@ -0,0 +1,11 @@
+# jenkins-dotnet-core
+[![](https://images.microbadger.com/badges/image/yahuiwong/jenkins-dotnet-core.svg)](https://microbadger.com/images/yahuiwong/jenkins-dotnet-core "Get your own image badge on microbadger.com")
+[![](https://images.microbadger.com/badges/version/yahuiwong/jenkins-dotnet-core.svg)](https://microbadger.com/images/yahuiwong/jenkins-dotnet-core "Get your own version badge on microbadger.com")
+
+1. sh init.sh
+
+2. docker-compose up -d
+
+3. Open ip:8180 on the browser 
+
+4. cat jenkins_home/secrets/initialAdminPassword & Set the initialAdminPassword string to your jenkins page
---
+++ install.sh
@@ -0,0 +1,18 @@
+# Based on instructiions at https://www.microsoft.com/net/download/linux-package-manager/debian9/sdk-current
+# Install dependency for .NET Core 2
+yum update -y
+yum install -y curl libunwind8 gettext apt-transport-https
+
+# Based on instructions at https://www.microsoft.com/net/download/linux-package-manager/debian9/sdk-current
+# Install microsoft.qpg & Install the .NET Core framework 
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/9/prod.list
+mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+
+yum update
+yum install -y dotnet-sdk-3.0
+yum clean
